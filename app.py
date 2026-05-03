18b5b58e51fdf273b61e78968d3197d146f829d1
Artpee <nijadack1234@gmail.com>
2026-05-04 00:26:39 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
+# หน้าแรก
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# หน้าแนะนำโครงงาน
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+# หน้า dashboard
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+# หน้าสินค้า
+import pandas as pd
+
+@app.route("/product")
+def product():
+     
+    return render_template("product.html")
+
+# หน้า creator
+@app.route("/creator")
+def creator():
+    return render_template("creator.html")
+
+# API ทดสอบ
+@app.route("/predict", methods=["POST"])
+def predict():
+    text = request.json["text"]
+
+    if "good" in text or "great" in text:
+        result = "Positive"
+    elif "bad" in text or "cheap" in text:
+        result = "Negative"
+    else:
+        result = "Neutral"
+
+    return jsonify({"prediction": result})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
